af39fcb74c1210bab28fd6a4a4c6054056422cde
Ivan <dangelzm@gmail.com>
2015-08-18 10:22:08 +0300

    Init commit

---
+++ slide_config.js
@@ -0,0 +1,27 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Введение в веб-компоненты,<br>библиотека Polymer',
+    subtitle: 'взгляд в будущее или будущее уже сегодня',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/geekTalks/geekTalks-logo.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Ivan Zmerzlyi',
+    company: 'Full stack web developer<br>Trinity Systems Ukraine',
+    gplus: 'https://google.com/+IvanZmerzlyi',
+    twitter: '@IvanZmerzlyi',
+    www: 'http://ivan.zmerzlyi.me'
+  }]
+};
+
